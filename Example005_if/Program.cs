4caa015ce1458065c2c7e0aae97e5d9e9bdf9180
Artyom <simstemasss@mail.ru>
2022-12-01 19:35:32 +0300

    Example005_if

---
+++ Example005_if/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите имя");
+String name =Console.ReadLine();
+if (name.ToLower() =="антон"){
+    Console.WriteLine("Суперское имя!");
+}else {Console.WriteLine("Привет " + name);}
