b8e8bb104eca91849055e52d01cb10414bb39c66
Artyom <simstemasss@mail.ru>
2022-12-03 20:43:08 +0300

    Example011

---
+++ Example011/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if ((a%b)==0){
+    System.Console.WriteLine("Число " + b + " кратно числу " + a);
+}else{
+    System.Console.WriteLine("Остаток от деления числа " + a+ " на число " + b + " = " + (a%b));
+}
